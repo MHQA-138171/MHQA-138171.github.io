e5144cf52ed85ad7ec18891b01e3cf505f52449d
MHQA-138171 <omegameen1@gmail.com>
2022-08-19 12:15:38 +0500

    logging api requests with morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,12 @@
 const path = require('path');
 
 const express = require('express');
+const morgan = require('morgan');
 
 const usersRouter = require('./routers/users.router');
 
 const app = express();
-
+app.use(morgan('combined'));
 app.use('/users', usersRouter);
 app.use(express.static(path.join(__dirname, '..', 'Public')));
 app.get('/*', (req, res) => {
